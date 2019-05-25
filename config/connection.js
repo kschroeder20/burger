1d4416bf63c7258b3517cd6fd3a08fa39e072ed4
Kevin Schroeder <schroederkevin94@gmail.com>
2019-05-25 11:48:33 -0400

    made edits for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // This file connects to the database
 var mysql = require("mysql");
-//connect to the burgers_db
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers",
-    insecureAuth: true
-});
+
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //connect to the burgers_db
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers",
+        insecureAuth: true
+    });
+};
 // db connection
 connection.connect(function (err) {
     if (err) {
